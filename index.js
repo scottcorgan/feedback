f4f45bd683c4cc0b463dbda0318c915a5a99e902
collin <collintmiller@gmail.com>
2013-11-05 12:25:16 -0600

    optionally strip colors

--- index.js
+++ index.js
@@ -1,38 +1,57 @@
-require('colors');
+var colors = require('colors');
 
 var feedback = {
   _msg: function (msg) {
     return msg || '';
   },
   
+  color: true,
+  
+  colorFix: function(msg) {
+    if(feedback.color) { return msg; }
+    return colors.stripColors(msg);
+  },
+  
   write: function (msg) {
     msg = msg || '';
     
-    process.stdout.write(feedback._msg(msg));
+    process.stdout.write(
+      feedback.colorFix(feedback._msg(msg))
+    );
     return this;
   },
   
   writeln: function (msg) {
-    feedback.write('\n' + feedback._msg(msg));
+    feedback.write(
+      feedback.colorFix('\n' + feedback._msg(msg))
+    );
     return this;
   },
   
   info: function (msg) {
-    return console.log(feedback._msg(msg));
+    return console.log(
+      feedback.colorFix(feedback._msg(msg))
+    );
   },
   
   success: function (msg) {
-    return console.log('Success:'.green + ' ' + feedback._msg(msg));
+    return console.log(
+      feedback.colorFix('Success:'.green + ' ' + feedback._msg(msg))
+    );
   },
   
   warn: function (msg) {
     msg = msg || '';
-    return console.log('Warning:'.yellow + ' ' + feedback._msg(msg));
+    return console.log(
+      feedback.colorFix('Warning:'.yellow + ' ' + feedback._msg(msg))
+    );
   },
   
   error: function (msg) {
     msg = msg || '';
-    return console.log('Error:'.red + ' ' + feedback._msg(msg));
+    return console.log(
+      feedback.colorFix('Error:'.red + ' ' + feedback._msg(msg))
+    );
   }
 };
 
